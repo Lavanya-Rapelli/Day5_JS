bd684b5dbbf3590ef224838c6d1f896bb62d719b
Lavanya Rapelli <lavanyarapelli555@gmail.com>
2024-07-28 22:26:25 +0530

    Basic javascript problmes

---
+++ problem1.js
@@ -0,0 +1,6 @@
+/* Print "Hello World!" in the javascript console followed by "Hello World! How are you doing!!" 
+in a different line.
+*/
+
+
+console.log("Hello World!" + "\n" + "How are you doing!!");
---
+++ problem4.js
@@ -0,0 +1,29 @@
+
+/* Store the name as String, school as String, grade as character, section as character, rollNo as int and the marks scored as int by the student in 3 subjects (Maths , Science and English)
+
+Print the report card of the student (You can make it look nice by using some keyboard symbols ), Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art) or Text Art (https://fsymbols.com/text-art/) for some inspiration
+
+*/
+
+console.log("*********************************************");
+console.log("                Report Card                  ");
+console.log("*********************************************");
+console.log("Student Name: Lavanya Rapelli");
+console.log("Student ID: ft38_416");
+console.log("Grade Level: unit 1");
+console.log("School Name: Masai School");
+console.log("*********************************************");
+console.log("Subjects                     Grades");
+console.log("*********************************************");
+console.log("Javascript                     A+");
+console.log("Python                         A");
+console.log("Java                           A+");
+console.log("MongoDB                        B+");
+console.log("React                          B");
+console.log("*********************************************");
+console.log("           End Of Report Card                ");
+console.log("*********************************************");
+
+
+
+
